10661540e213c418df3ca74c8d3438e17370dc8d
MW <Mathe@192.168.2.105>
2017-01-14 15:57:41 +0100

    Reset to Master
    creating Class MappingGraph

---
+++ scopviz/src/main/java/de/tu_darmstadt/informatik/tk/scopviz/main/MappingGraph.java
@@ -0,0 +1,12 @@
+package de.tu_darmstadt.informatik.tk.scopviz.main;
+
+import org.graphstream.graph.Graph;
+
+public class MappingGraph extends MyGraph {
+
+	public MappingGraph(String id, Graph underlay, Graph operator) {
+		super(id);
+		
+	}
+
+}
